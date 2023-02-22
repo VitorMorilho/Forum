34a575ad19a4e3e983624034e8e502400d6482c0
VitorMorilho <vitormorilho@hotmail.com>
2023-02-22 15:19:54 -0300

    Adiciona tratamento de erro 400.
    Foi tratado também a devolutiva das mensagens para melhor compreensão de cadastro do usuário.

--- src/main/kotlin/br/com/alura/forum/model/to/NovoTopicoFormTO.kt
+++ src/main/kotlin/br/com/alura/forum/model/to/NovoTopicoFormTO.kt
@@ -5,8 +5,13 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class NovoTopicoFormTO(
-    @field:NotEmpty @field:Size(min = 5, max = 100) val titulo :String,
-    @field:NotEmpty val menssagem: String,
-    @field:NotNull val idCurso: Long,
-    @field:NotNull val idUsuario: Long
+    @field:NotEmpty(message = "O campo titulo não pode ser vazio")
+    @field:Size(min = 5, max = 100, message = "Titulo deve ter entre 5 a 100 caracteres")
+    val titulo :String,
+    @field:NotEmpty(message = "O campo mensagem não pode ser vazio")
+    val menssagem: String,
+    @field:NotNull(message = "O campo idCurso não pode ser vazio")
+    val idCurso: Long,
+    @field:NotNull(message = "O campo idUsuario não pode ser vazio")
+    val idUsuario: Long
 )
